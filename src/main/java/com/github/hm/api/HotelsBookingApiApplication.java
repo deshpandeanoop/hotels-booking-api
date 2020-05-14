eeae6ae658e91cbc9809965ab7244ee9b2d55ad7
deshpandeanoop <anoop.bhardwaja@gmail.com>
2020-05-14 14:57:37 +0530

    Initial commit

---
+++ src/main/java/com/github/hm/api/HotelsBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.hm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsBookingApiApplication.class, args);
+	}
+
+}
